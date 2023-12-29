e5d8101e7e5f270083726b4408e0b35f5f0ac635
moriyad12 <mohamedelhenawy2012@yahoo.com>
2023-12-29 02:55:02 +0200

    adding headers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Pet from "./Pet/Pet";
+import PetHeader from "./Headers/PetHeader";
+import ApplicationHeader from "./Headers/ApplicationHeader";
 
 function App() {
   return (
     <div className="App">
-      <Pet />
+      <Pet/>
     </div>
   );
 }
---
+++ src/Headers/ApplicationHeader.js
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+import Typography from '@mui/material/Typography';
+import {Button, CardActionArea, CardActions} from '@mui/material';
+
+
+function ApplicationHeader(props) {
+    let ApplicationHeader = props.ApplicationHeader;
+    // const ApplicationHeader = {
+    //         adpterId: 1,
+    //         petId: 1,
+    //         adopterName: "Sample Adopter",
+    //         petName: "Sample Pet",
+    //         status: "Pending",
+    //         shelterName: "Sample Shelter",
+    //         applicationDate: "2021-10-10"
+    // };
+
+
+    const handleSeeMore = () => {
+        const params = {
+            id: ApplicationHeader.id,
+        };
+    }
+
+    return (
+        <Card className="card" style={{width: "90%", transition: "all 0.2s ease-in-out"}}>
+            <CardActionArea>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        <div className="card-header">
+                            <div className="pet-name">
+                                {ApplicationHeader.adopterName}
+                            </div>
+                        </div>
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        <div className="card-content">
+                            <div className="pet-category">
+                                Gender: {ApplicationHeader.status}
+                            </div>
+                            <div className="location">
+                                Species: {ApplicationHeader.petName}
+                            </div>
+                            <div className="location">
+                                applicationDate: {ApplicationHeader.applicationDate}
+                            </div>
+                        </div>
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <div className="card-footer">
+                    <div className="Organizer-name">
+                        Shelter Name: {ApplicationHeader.shelterName}
+                    </div>
+                </div>
+            </CardActions>
+        </Card>
+    );
+
+}
+
+export default ApplicationHeader;
