a3a98bde2482dce43645e7d9f3a7da340805696b
faris <113785816+faris0007@users.noreply.github.com>
2023-12-29 02:43:53 +0200

    profile but need to modify update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import React from "react";
+import Profile from "./Profile/Profile";
 function App() {
   return (
     <div className="App">
-
+        <Profile> </Profile>
     </div>
   );
 }
