82bdf47b094d75d10712c2466de286cceb829909
Ahmed Hassan <ahmedmohamedhassan1to10@gmail.com>
2023-12-29 02:21:28 +0200

    to do

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Login from "./loginAndSignup/login";
+import ValidationPage from "./validation/validationPage";
 function App() {
   return (
     <div className="App">
-
+        <Login/>
     </div>
   );
 }
