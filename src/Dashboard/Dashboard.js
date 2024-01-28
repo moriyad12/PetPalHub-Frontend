50ed7bff7765fc9e7aee8f04a340347d878a2c58
Michael <michaael.monir119@gmail.com>
2024-01-28 16:23:37 +0200

    layout fixed and now heading for adding the filter component

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import "./Other.css"
+import "../MyUtilities/Colors.css"
 import { allAnimals } from "./AllAnimals";
 import CardsSquareView from "./CardsView/CardsSquareView";
 
@@ -8,12 +9,16 @@ function Dashboard({filterEnabled, viewComponentIndex}) {
     return(
         <div className="container-fluid">
             <div className="row">
-                <div className="col-9 pe-1">
+                <div className="col-2 bg-light-grey">
+                    {/*<img src={require("./ff4.jpg")} className="dashboard-image"/>*/}
+                    <h1 className="left-text">Find your perfect companion and make a forever friend</h1>
+                </div>
+                <div className="col-8 pe-1">
                     <div className="bg-light-grey p-3">
-                        <CardsSquareView cards={allAnimals} />
+                        <CardsSquareView cards={allAnimals}/>
                     </div>
                 </div>
-                <div className="col-3 ps-1">
+                <div className="col-2 ps-1">
                     <div className="bg-light-grey p-3 my-filter">
                         <div className="scroll-content">
                             <h1>Right Container (20%)</h1>
